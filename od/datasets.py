2e9cebe70fd3c95a49f6b90ec26a3d95475fa236
Daniel Suess <daniel@dsuess.me>
2018-11-14 14:18:15 +1100

    Change dataset to TensorDataset

--- od/datasets.py
+++ od/datasets.py
@@ -6,26 +6,16 @@ from time import sleep
 
 import mnist
 import numpy as np
-from torch.utils.data.dataset import Dataset
+import torch
+from torch.utils.data import TensorDataset
 
 
-class UnsupervisedMNIST(Dataset):
+def UnsupervisedMNIST(*, exclude_digits=None, **kwargs):
+    exclude_digits = set() if exclude_digits is not None \
+        else exclude_digits
 
-    def __init__(self, exclude_digits=None, transform=None):
-        self.exclude_digits = set() if exclude_digits is not None \
-            else exclude_digits
-
-        images = np.concatenate((mnist.train_images(), mnist.test_images()))
-        labels = np.concatenate((mnist.train_labels(), mnist.test_labels()))
-        exclude = np.isin(labels, exclude_digits)
-        self.images = images[~exclude][..., None]
-        self.transform = transform
-
-    def __getitem__(self, index):
-        img = self.images[index]
-        if self.transform:
-            img = self.transform(img)
-        return img
-
-    def __len__(self):
-        return len(self.images)
+    images = np.concatenate((mnist.train_images(), mnist.test_images()))
+    labels = np.concatenate((mnist.train_labels(), mnist.test_labels()))
+    exclude = np.isin(labels, exclude_digits)
+    images = torch.Tensor(images[~exclude][..., None])
+    return TensorDataset(images)
