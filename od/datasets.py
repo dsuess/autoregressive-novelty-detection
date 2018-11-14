996d5cdce573600b7f7db5892bf7d6b6701b06b1
Daniel Suess <daniel@dsuess.me>
2018-11-15 10:55:04 +1100

    Properly normalize UnsupervisedMNIST dataset

--- od/datasets.py
+++ od/datasets.py
@@ -10,12 +10,15 @@ import torch
 from torch.utils.data import TensorDataset
 
 
-def UnsupervisedMNIST(*, exclude_digits=None, **kwargs):
+def UnsupervisedMNIST(*, exclude_digits=None):
     exclude_digits = set() if exclude_digits is not None \
         else exclude_digits
 
-    images = np.concatenate((mnist.train_images(), mnist.test_images()))
+    # we have to normalize to [0, 1] since output layer uses sigmoid
+    images = np.concatenate((mnist.train_images(), mnist.test_images())) / 255
+
     labels = np.concatenate((mnist.train_labels(), mnist.test_labels()))
     exclude = np.isin(labels, exclude_digits)
-    images = torch.Tensor(images[~exclude][..., None])
+    images = torch.Tensor(images[~exclude][..., None]).permute(0, 3, 1, 2)
+
     return TensorDataset(images)
