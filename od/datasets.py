81b5588ed3f119deb00993c967721f88570faa69
Daniel Suess <daniel@dsuess.me>
2018-11-14 13:28:46 +1100

    Implement basic MNIST dataset

--- od/datasets.py
+++ od/datasets.py
@@ -1,30 +1,31 @@
 import functools as ft
+import logging
 from operator import add
 from pathlib import Path
-import logging
-
-from torch.utils.data.dataset import Dataset
 from time import sleep
-from PIL import Image
+
+import mnist
+import numpy as np
+from torch.utils.data.dataset import Dataset
 
 
-class UnsupervisedImageDataset(Dataset):
+class UnsupervisedMNIST(Dataset):
 
-    def __init__(self, directory, extensions=['jpg'], transform=None):
-        directory = Path(directory)
-        self.paths = list(
-            ft.reduce(add, [directory.glob(f'*.{ext}') for ext in extensions]))
+    def __init__(self, exclude_digits=None, transform=None):
+        self.exclude_digits = set() if exclude_digits is not None \
+            else exclude_digits
+
+        images = np.concatenate((mnist.train_images(), mnist.test_images()))
+        labels = np.concatenate((mnist.train_labels(), mnist.test_labels()))
+        exclude = np.isin(labels, exclude_digits)
+        self.images = images[~exclude][..., None]
         self.transform = transform
-        logging.info(f'Found {len(self.paths)} images in {directory}')
-
-    def __len__(self):
-        return len(self.paths)
 
     def __getitem__(self, index):
-        path = self.paths[index]
-        img = Image.open(path).convert('RGB')
-
+        img = self.images[index]
         if self.transform:
             img = self.transform(img)
-
         return img
+
+    def __len__(self):
+        return len(self.images)
