c5b06355ebf13bc1c2126af23bbee1e27c37e212
Daniel Suess <daniel@dsuess.me>
2018-11-22 08:48:11 +1100

    Add more evaluation metrics to tensorboard

--- od/experiments.py
+++ od/experiments.py
@@ -4,11 +4,10 @@ from multiprocessing import cpu_count
 from pathlib import Path
 
 import click
+import matplotlib.pyplot as pl
 import numpy as np
+import od
 import torch
-from od.autoregress import AutoregresionModule, AutoregressiveLoss
-from od.datasets import UnsupervisedMNIST
-from od.encoders import ResidualAE
 from od.utils import logger
 from tensorboardX import SummaryWriter
 from torch import nn
@@ -30,7 +29,7 @@ class Experiment:
     Datasets = namedtuple('Datasets', 'train, test')
 
     def __init__(self, datasets, conv_channels, fc_channels, mfc_channels,
-                 batch_size, nr_epochs, logdir, settings):
+                 batch_size, nr_epochs, logdir, settings, ar_weight):
         self.datasets = self.Datasets(*datasets)
         logger.info(f'Got {len(self.datasets.train)} training examples and '
                     f'{len(self.datasets.test)} test examples')
@@ -40,22 +39,23 @@ class Experiment:
             DataLoader(self.datasets.test, batch_size=batch_size,
                        num_workers=2 * cpu_count()))
         self.sample_images = self.Datasets(
-            sample_examples(self.datasets.train, size=50),
-            sample_examples(self.datasets.test, size=50))
+            sample_examples(self.datasets.train, size=10),
+            sample_examples(self.datasets.test, size=10))
 
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         logger.info(f'Using pytorch device={self.device}')
 
         color_channels, *input_shape = self.datashape
-        encoder = ResidualAE(
+        encoder = od.ResidualAE(
             input_shape, conv_channels, fc_channels,
             color_channels=color_channels, latent_activation=nn.Sigmoid())
-        regressor = AutoregresionModule(
+        regressor = od.AutoregresionModule(
             fc_channels[-1], mfc_channels,
             batchnorm=settings.get('autoregress_batchnorm', True))
-        model = AutoregressiveLoss(encoder, regressor)
+        model = od.AutoregressiveLoss(encoder, regressor)
         self.model = model.to(self.device)
         self.optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+        self.ar_weight = ar_weight
 
         Path(logdir).mkdir(exist_ok=True)
         self.nr_epochs = nr_epochs
@@ -98,7 +98,7 @@ class Experiment:
             losses = self.model(x)
             losses = self.model.Result(
                 losses.reconstruction.mean(), losses.autoregressive.mean())
-            loss = losses.reconstruction + losses.autoregressive
+            loss = losses.reconstruction + self.ar_weight * losses.autoregressive
 
             loss_summary['loss/total'] += loss
             loss_summary['loss/reconstruction'] += losses.reconstruction
@@ -112,29 +112,42 @@ class Experiment:
         for name, value in loss_summary.items():
             summary_writer.add_scalar(name, value / nr_examples, epoch)
 
+    def make_example_images(self, sample_images):
+        imgs_pred = self.model.encoder(sample_images.to(self.device))
+        img_pairs = zip(sample_images, imgs_pred.cpu())
+        img_merged = (make_grid(list(pair), nrow=1) for pair in img_pairs)
+        all_images = make_grid(list(img_merged), nrow=len(sample_images))
+        return all_images
+
+
     def eval_epoch(self, epoch, summary_writer):
         self.model.eval()
-        imgs_pred = self.model.encoder(self.sample_images.train.to(self.device))
-        img_pairs = zip(self.sample_images.train, imgs_pred.cpu())
-        for n, img_pair in enumerate(img_pairs):
-            merged = make_grid(list(img_pair))
-            summary_writer.add_image(f'train_{n}', merged, epoch)
+        with torch.no_grad():
+            examples = self.make_example_images(self.sample_images.train)
+            summary_writer.add_image('train_images', examples, epoch)
+            examples = self.make_example_images(self.sample_images.test)
+            summary_writer.add_image('test_images', examples, epoch)
 
-        imgs_pred = self.model.encoder(self.sample_images.test.to(self.device))
-        img_pairs = zip(self.sample_images.test, imgs_pred.cpu())
-        for n, img_pair in enumerate(img_pairs):
-            merged = make_grid(list(img_pair))
-            summary_writer.add_image(f'test_{n}', merged, epoch)
+            train_losses = np.concatenate(
+                [self.model.predict(x.to(self.device)).to('cpu').numpy()
+                 for x, in self.loaders.train])
+            test_losses = np.concatenate(
+                [self.model.predict(x.to(self.device)).to('cpu').numpy()
+                 for x, in self.loaders.test])
+
+            fig = pl.figure(0, figsize=(10, 10))
+            pl.hist(train_losses, bins=100, density=True)
+            pl.hist(test_losses, alpha=0.5, bins=100, density=True)
+            rendered_fig = od.utils.render_mpl_figure()
+            summary_writer.add_image('loss_histogram', rendered_fig, epoch)
+
+            overlap = od.utils.sample_distribution_overlap(train_losses, test_losses)
+            summary_writer.add_scalar('histogram_overlap', overlap, epoch)
+
 
     def run(self):
         summary_writer = SummaryWriter(self.logdir)
         restored_epoch = self.restore_latest(self.checkpoint_dir)
-        #  if restored_epoch <= 0:
-        #      # Batch size == 1 fails due to batch norm in training mode
-        #      self.model.eval()
-        #      dummy_input = torch.Tensor(1, *self.datashape).to(self.device)
-        #      summary_writer.add_graph(self.model, dummy_input)
-
         self.eval_epoch(0, summary_writer)
         epochs = tqdm(range(restored_epoch + 1, self.nr_epochs + 1),
                       total=self.nr_epochs, initial=restored_epoch)
@@ -163,8 +176,8 @@ def mnist(logdir):
     all_digits = set(range(10))
     test_digits = {3, 5, 8}
     train_digits = all_digits.difference(test_digits)
-    ds_train = UnsupervisedMNIST(exclude_digits=test_digits)
-    ds_test = UnsupervisedMNIST(exclude_digits=train_digits)
+    ds_train = od.UnsupervisedMNIST(exclude_digits=test_digits)
+    ds_test = od.UnsupervisedMNIST(exclude_digits=train_digits)
 
     Experiment(
         datasets=(ds_train, ds_test),
@@ -174,4 +187,5 @@ def mnist(logdir):
         batch_size=64,
         nr_epochs=50,
         logdir=logdir,
-        settings={'autoregress_batchnorm': False}).run()
+        settings={'autoregress_batchnorm': False},
+        ar_weight=10).run()
--- od/utils.py
+++ od/utils.py
@@ -1,6 +1,9 @@
 import logging
 import os
 
+import matplotlib.backends.backend_agg as pl_backend_agg
+import matplotlib.pyplot as pl
+import numpy as np
 
 __all__ = ['get_default_logger', 'logger']
 
@@ -9,6 +12,39 @@ def get_default_logger(name):
     logger = logging.getLogger(name)
     logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'))
     return logger
+
+
+def render_dict_as_table(the_dict):
+    lines = ['Key | Value', '--- | ---']
+    lines += [f'{key} | {val}' for key, val in the_dict.items()]
+    return lines
+
+
+def sample_distribution_overlap(xs1, xs2, nr_bins=200):
+    vmin = min(np.min(xs1), np.min(xs2))
+    vmax = max(np.max(xs1), np.max(xs2))
+    bins = np.linspace(vmin, vmax, nr_bins)
+    y1, _ = np.histogram(xs1, bins=bins, density=True)
+    y2, _ = np.histogram(xs2, bins=bins, density=True)
+    return np.sum(np.min([y1, y2], axis=0)) * (vmax - vmin ) / nr_bins
+
+
+def render_mpl_figure(figure=None, close_figure=True, channel_order='CHW'):
+    assert channel_order in {'CHW', 'HWC'}
+    figure = figure if figure is not None else pl.gcf()
+    canvas = pl_backend_agg.FigureCanvasAgg(figure)
+    canvas.draw()
+    data = np.frombuffer(canvas.buffer_rgba(), dtype=np.uint8)
+    w, h = figure.canvas.get_width_height()
+    image = data.reshape([h, w, 4])[:, :, 0:3]
+
+    if channel_order == 'CHW':
+        image = image.transpose(2, 0, 1)
+
+    if close_figure:
+        pl.close(figure)
+
+    return image
 
 
 logger = get_default_logger('AND')
