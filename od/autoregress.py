1986abfef21f00a04e6b8e15c83b0f0be443e48a
Daniel Suess <daniel@dsuess.me>
2018-11-21 18:03:10 +1100

    Make autoregression loss work

--- od/autoregress.py
+++ od/autoregress.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from torch import nn
 
+from .utils import logger
 
 __all__ = ['AutoregresionModule', 'AutoregressiveLoss']
 
@@ -11,18 +12,20 @@ __all__ = ['AutoregresionModule', 'AutoregressiveLoss']
 # TODO Optimize axis alignment to get rid of permute in loss
 class AutoregressiveLayer(nn.Linear):
     def __init__(self, dim, in_features, out_features, *, mask_type,
-                 batch_norm=True, activation=None, **kwargs):
+                 batchnorm=True, activation=None, **kwargs):
         super().__init__(dim * in_features, dim * out_features, **kwargs)
         assert mask_type in {'A', 'B'}
         self.dim = dim
         self.in_features = in_features
         self.out_features = out_features
         self.activation = activation
+        self.mask_type = mask_type
 
-        if batch_norm:
-            self.batch_norm = nn.BatchNorm1d(dim * out_features)
+        if batchnorm:
+            self.batchnorm = nn.BatchNorm1d(dim * out_features)
+            logger.info('Using batchnorm for AutoregressiveLayer')
         else:
-            self.batch_norm = lambda x: x
+            self.batchnorm = lambda x: x
 
         self.in_shape = (dim * in_features,)
         self.out_shape = (dim, out_features)
@@ -37,7 +40,7 @@ class AutoregressiveLayer(nn.Linear):
     def forward(self, x):
         self.weight.data *= self.mask
         y = super().forward(x.view(-1, *self.in_shape))
-        y = self.batch_norm(y)
+        y = self.batchnorm(y)
 
         if self.activation is not None:
             y = self.activation(y)
@@ -47,20 +50,23 @@ class AutoregressiveLayer(nn.Linear):
     def __repr__(self):
         return f'AutoregressiveLayer(dim={self.dim}, ' \
             f'in_features={self.in_features}, ' \
-            f'out_features={self.out_features})'
+            f'out_features={self.out_features}, ' \
+            f'mask_type={self.mask_type})'
 
 
 class AutoregresionModule(nn.Module):
-    def __init__(self, dim, mfc_layers, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, dim, mfc_layers, activation=nn.functional.leaky_relu,
+                 **kwargs):
+        super().__init__()
         self.mfc_layers = list(mfc_layers)
-        dimensions = list(zip([1] + self.mfc_layers, self.mfc_layers[1:]))
-        activation_fns = [nn.functional.leaky_relu] * (len(dimensions) - 1) + [None]
+        dimensions = list(zip([1] + self.mfc_layers, self.mfc_layers))
+        activation_fns = [activation] * (len(dimensions) - 1) + [None]
         mask_types = ['A'] + ['B'] * (len(dimensions) - 1)
         configuration = zip(dimensions, activation_fns, mask_types)
 
         self.layers = nn.Sequential(
-            *[AutoregressiveLayer(dim, d_in, d_out, activation=fn, mask_type=mt)
+            *[AutoregressiveLayer(dim, d_in, d_out, activation=fn,
+                                  mask_type=mt, **kwargs)
               for (d_in, d_out), fn, mt in configuration])
 
     @property
@@ -80,36 +86,32 @@ class AutoregressiveLoss(nn.Module):
         self.encoder = encoder
         self.regressor = regressor
 
-        self.register_scalar('reconstruction_normalization',
-                             np.prod(encoder.input_shape), torch.float)
-        self.register_scalar('bins', self.regressor.bins - 1, torch.int64)
+        self.register_scalar('bins', self.regressor.bins, torch.int64)
 
     def register_scalar(self, name, val, type):
         val = torch.Tensor([val]).reshape(tuple()).to(type)
         self.register_buffer(name, val)
 
+    def _autoreg_loss(self, latent):
+        latent_binned = (latent * self.bins.float()).type(torch.int64)
+        latent_binned = latent_binned.clamp(0, self.bins - 1)
+        latent_binned_pred = self.regressor(latent).permute(0, 2, 1)
+        autoreg_loss = nn.functional.cross_entropy(
+            latent_binned_pred, latent_binned, reduction='none')
+        return autoreg_loss.mean(dim=1)
+
     def forward(self, x):
         latent = self.encoder.encode(x)
         reconstruction = self.encoder.decode(latent)
-        reconstruction_loss = nn.functional.mse_loss(x, reconstruction)
-        reconstruction_loss /= self.reconstruction_normalization
+        reconstruction_loss = nn.functional.mse_loss(
+            x, reconstruction, reduction='none')
+        reconstruction_loss = reconstruction_loss.view(reconstruction_loss.size(0), -1)
+        reconstruction_loss = reconstruction_loss.sum(dim=1)
 
-        latent_binned = (latent * self.bins.float()).type(torch.int64)
-        latent_binned = torch.max(input=latent_binned , other=self.bins)
-        latent_binned_pred = self.regressor(latent).permute(0, 2, 1)
-        autoreg_loss = nn.functional.cross_entropy(
-            latent_binned_pred, latent_binned)
-
+        autoreg_loss = self._autoreg_loss(latent)
         return self.Result(reconstruction_loss, autoreg_loss)
 
-    def predict(self, x, retrecons=False):
+    def predict(self, x):
         with torch.no_grad():
             latent = self.encoder.encode(x)
-            logits = self.regressor(latent)
-            prob = torch.softmax(logits, dim=2)
-
-            if retrecons:
-                recons = self.encoder.decode(latent)
-                return prob, recons
-            else:
-                return prob
+            return self._autoreg_loss(latent)
