6cb88f15170417c0f31f7c601b120c88ea683dd3
Daniel Suess <daniel@dsuess.me>
2018-11-22 17:29:36 +1100

    Fix crash due to too small ulimit

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from od.experiments import experiments
+import resource
 
 
 if __name__ == '__main__':
+    rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+    resource.setrlimit(resource.RLIMIT_NOFILE, (2048, rlimit[1]))
     experiments()
