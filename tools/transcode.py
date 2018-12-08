132be44d00dd801368c8b3e9e181d83825b6e8ed
Daniel Suess <daniel@dsuess.me>
2018-12-08 15:08:35 +1100

    Make transcode work with single files

--- tools/transcode.py
+++ tools/transcode.py
@@ -3,6 +3,7 @@ import click
 from pathlib import Path
 from collections import defaultdict
 from tempfile import TemporaryDirectory
+from glob import glob
 
 
 @click.group('main')
@@ -25,33 +26,17 @@ def merge_video_pipeline(paths, frame_interval):
     return ffmpeg.concat(*single_pipelines)
 
 
-
-
 @main.command('shanghai-tech')
-@click.option('--input-dir', required=True, type=click.Path(file_okay=False, resolve_path=True, writable=False))
-@click.option('--output-dir', required=True, type=click.Path(file_okay=False, resolve_path=True, writable=True))
+@click.option('--input-path', required=True)
+@click.option('--output-path', required=True, type=click.Path(file_okay=True, resolve_path=True, writable=True))
 @click.option('--frame-interval', default=16, type=int)
-def shanghai_tech(input_dir, output_dir, frame_interval):
-    input_dir = Path(input_dir)
-    output_dir = Path(output_dir)
-
-    matches = input_dir.glob('*.avi')
-    grouped_matches = defaultdict(list)
-    for match in matches:
-        key = str(match.stem).split('_')[0]
-        grouped_matches[key].append(str(match))
-    output_dir.mkdir(exist_ok=True)
+def shanghai_tech(input_path, output_path, frame_interval):
+    matches = glob(input_path)
     encode_args = {
-        'c:v': 'libx264', 'crf': '18', 'pix_fmt': 'yuv420p',
-        'x264-params': f'keyint={frame_interval}',
-        'profile:v': 'high',
-    }
-
-    for key, paths in grouped_matches.items():
-        merged_pipeline = merge_video_pipeline(paths, frame_interval)
-        merged_pipeline \
-            .output(str(output_dir / f'{key}.mp4'), **encode_args) \
-            .run()
+        'c:v': 'libx264', 'crf': '18', 'pix_fmt': 'yuv420p', 'g': f'{frame_interval}',
+        'profile:v': 'high', 'x264-params': f'keyint_min={frame_interval // 2}'}
+    stream = merge_video_pipeline(list(matches), frame_interval)
+    stream.output(output_path, **encode_args).run()
 
 
 if __name__ == '__main__':
