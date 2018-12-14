3132e3994b21af8f22404aa1cfb765c5ceaa5a04
Daniel Suess <daniel@dsuess.me>
2018-12-15 10:44:51 +1100

    Add transcode script for testing videos

--- tools/transcode.py
+++ tools/transcode.py
@@ -6,7 +6,7 @@ from tempfile import TemporaryDirectory
 from glob import glob
 
 
-@click.group('main')
+@click.group('shanghai_tech')
 def main():
     pass
 
@@ -26,17 +26,44 @@ def merge_video_pipeline(paths, frame_interval):
     return ffmpeg.concat(*single_pipelines)
 
 
-@main.command('shanghai-tech')
+@main.command('train')
 @click.option('--input-path', required=True)
 @click.option('--output-path', required=True, type=click.Path(file_okay=True, resolve_path=True, writable=True))
 @click.option('--frame-interval', default=16, type=int)
-def shanghai_tech(input_path, output_path, frame_interval):
+@click.option('--resize-shape', type=str)
+def shanghai_tech_train(input_path, output_path, frame_interval, resize_shape):
     matches = glob(input_path)
     encode_args = {
         'c:v': 'libx264', 'crf': '18', 'pix_fmt': 'yuv420p', 'g': f'{frame_interval}',
         'profile:v': 'high', 'x264-params': f'keyint_min={frame_interval // 2}'}
     stream = merge_video_pipeline(list(matches), frame_interval)
+    if resize_shape is not None:
+        width, height = resize_shape.split(':')
+        stream = stream.filter('scale', width=width, height=height)
     stream.output(output_path, **encode_args).run()
+
+
+@main.command('test')
+@click.option('--testing-dir', required=True)
+@click.option('--output-dir', required=True, type=click.Path(file_okay=True, resolve_path=True, writable=True))
+@click.option('--frame-interval', default=16, type=int)
+@click.option('--resize-shape', type=str)
+def shanghai_tech_test(testing_dir, output_dir, frame_interval, resize_shape):
+    encode_args = {
+        'c:v': 'libx264', 'crf': '18', 'pix_fmt': 'yuv420p', 'g': f'{frame_interval}',
+        'profile:v': 'high', 'x264-params': f'keyint_min={frame_interval // 2}'}
+    output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
+    for sequence_dir in Path(testing_dir).glob('*'):
+        stream = ffmpeg.input(str(sequence_dir / '*.jpg'), pattern_type='glob',
+                              framerate=25)
+        if resize_shape is not None:
+            width, height = resize_shape.split(':')
+            stream = stream.filter('scale', width=width, height=height)
+
+        output_path = output_dir / f'{sequence_dir.name}.mp4'
+        stream.output(str(output_path), **encode_args).run()
 
 
 if __name__ == '__main__':
